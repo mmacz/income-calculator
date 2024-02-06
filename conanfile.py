f5d441d2339007a1b93d2d4b82524e758cf6f81b
Marek Macznik <marek.macznik@gmail.com>
2024-02-07 00:48:37 +0100

    Simplify conanfile

--- conanfile.py
+++ conanfile.py
@@ -17,18 +17,9 @@ class IncomeCalculator(ConanFile):
         "testing": True,
     }
 
-    def requirements(self):
-        pass
-
     def build_requirements(self):
         if self.options.testing:
             self.build_requires("gtest/1.10.0")
-
-    def validate(self):
-        pass
-
-    def configure(self):
-        pass
 
     def build(self):
         cmake = CMake(self)
@@ -36,9 +27,3 @@ class IncomeCalculator(ConanFile):
         cmake.build()
         if self.options.testing:
             cmake.test()
-
-    def package(self):
-        return super.package()
-
-    def package_info(self):
-        return super().package_info()
